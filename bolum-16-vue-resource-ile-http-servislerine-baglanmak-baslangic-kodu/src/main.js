f5c04c766b005648fadd276dbdacede4f5f9d417
hatice <haticekatranci@yandex.com>
2022-11-14 16:10:59 +0300

    first comm

---
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from "vue-resource";
+
+Vue.use(VueResource);
+
+Vue.http.options.root = "https://vuejs-vue-resource-28c02-default-rtdb.firebaseio.com/users.json";
+
+Vue.http.interceptors.push((request, next) => {
+ /* if(request.method == "POST"){
+    request.method = "PUT"
+  }
+  next();*/
+  next(response => {
+    response.json = () => {
+      return {
+        userList : response.body
+      }
+    }
+  });
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
