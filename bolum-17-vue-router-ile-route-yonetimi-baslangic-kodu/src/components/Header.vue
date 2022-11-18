fc85b849bae157574502b9f72c20b0b7d448517f
hatice <haticekatranci@yandex.com>
2022-11-18 18:45:46 +0300

    add router link id

--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/Header.vue
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/Header.vue
@@ -1,12 +1,7 @@
 <template>
   <ul class="nav nav-pills">
-    <router-link
-        to=""
-        tag="li">
-      <a>Home</a>
-    </router-link>
-
-
-    <li><a href="">User</a></li>
+    <router-link active-class="active" exact to="/" tag="li"><a>Home</a></router-link>
+    <router-link active-class="active" exact to="/user/1" tag="li"><a>User 1</a></router-link>
+    <router-link active-class="active" exact to="/user/2" tag="li"><a>User 2</a></router-link>
   </ul>
 </template>
--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/routes.js
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/routes.js
@@ -2,6 +2,6 @@ import Home from "./components/Home.vue";
 import User from "./components/user/User.vue";
 
 export const routes = [
-    { path : '', component : Home },
-    { path : '', component: User }
+    { path : '', component : Home , name : 'anasayfa' },
+    { path : '/user/:id', component: User, name : 'kullanici' }
 ]
