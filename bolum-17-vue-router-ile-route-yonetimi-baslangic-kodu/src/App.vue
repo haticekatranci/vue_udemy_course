73a5a962aa0f9aca2c20107c17be6c4d1c65da75
hatice <haticekatranci@yandex.com>
2022-11-18 17:08:09 +0300

    add router link

--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/App.vue
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/App.vue
@@ -1,8 +1,22 @@
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <br>
+        <app-header></app-header>
+        <hr>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
+  components : {
+    appHeader : Header
+  }
 }
 </script>
 
---
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link
+        to=""
+        tag="li">
+      <a>Home</a>
+    </router-link>
+
+
+    <li><a href="">User</a></li>
+  </ul>
+</template>
