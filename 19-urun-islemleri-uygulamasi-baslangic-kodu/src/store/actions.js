aa397429ffa4080d649e049ea9cba1ada69c0b8d
hatice <haticekatranci@yandex.com>
2022-12-15 17:14:39 +0300

    add update total purchase

--- 19-urun-islemleri-uygulamasi-baslangic-kodu/src/store/actions.js
+++ 19-urun-islemleri-uygulamasi-baslangic-kodu/src/store/actions.js
@@ -1,4 +1,14 @@
-export const setTradeResult = ({state}, ) => {
+import Vue from "vue";
+export const setTradeResult = ({state, commit}, tradeResult ) => {
+    commit("updateTradeResult" , tradeResult );
+    let tradeData = {
+        purchase : state.purchase,
+        sale : state.sale
+    }
+    Vue.http.put("https://urun-islemleri-prod-fdfde-default-rtdb.firebaseio.com/trade-result.json", tradeData )
+        .then(response => {
+            console.log(response);
+        })
 
 }
 export const getTradeResult = ({commit}) => {
--- 19-urun-islemleri-uygulamasi-baslangic-kodu/src/store/modules/product.js
+++ 19-urun-islemleri-uygulamasi-baslangic-kodu/src/store/modules/product.js
@@ -1,34 +1,43 @@
+import Vue from "vue"
 const state = {
     products : []
-
 }
 const getters = {
     getProducts(state){
         return state.products;
     },
     getProduct(state){
-
     }
-
 }
 const mutations = {
     updateProductList(state,product){
         state.products.push(product);
-
     }
 }
 const actions = {
     initApp({ commit }){
         //Vue resource işlemleri...
     },
-    saveProduct({ commit }, payload){
+    saveProduct({ dispatch, commit }, product){
         //Vue resource işlemleri...
+        Vue.http.post("https://urun-islemleri-prod-fdfde-default-rtdb.firebaseio.com/products.json", product)
+            .then((response) => {
+                ////******* Ürün Lİstesinin Güncellenmesi   ******///////
+                product.id = response.body.name;
+                commit("updateProductList", product);
+                ////*******Alış Satış BAkiye Bilgilerinin Güncellenmesi ********/////
+                let tradeResult = {
+                    purchase : product.price,
+                    sale : 0,
+                    count : product.count,
+                }
+                dispatch("setTradeResult", tradeResult)
+            })
 
     },
     sellProduct({ commit } ,payload){
         //Vue resource işlemleri...
     }
-
 }
 export default {
     state,
